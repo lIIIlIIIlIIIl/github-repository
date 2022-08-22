8344097f50e7a3d0245060811958c6e2caa2b55f
lIIIlIIIlIIIl <sdhsdj6450@gmail.com>
2022-08-22 14:56:14 +0900

    First-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <form>
+        <label>할 일을 입력해주세요.</label>
+        <input />
+        <button>작성하기</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
